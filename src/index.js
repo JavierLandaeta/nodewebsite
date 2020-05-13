065a40974d70167024359f51215236a314283800
JavierLandaeta <ingjavierlandaeta@hotmail.com>
2020-05-13 18:46:59 +0200

    Este es un nuevo html para cargar archivos

--- src/index.js
+++ src/index.js
@@ -9,12 +9,12 @@ app.set("port", 80)
 // routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'))
-    
 
 })
 // static files
 
 //Listening the server
 app.listen(app.get("port"), () => {
+    console.log(__dirname);
     console.log("Server on port", app.get("port"));
 })
